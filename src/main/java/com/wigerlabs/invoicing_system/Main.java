71ef7fffaeef69eb61d03ddb60f34607559a1ce0
Maleesha Pramud <maleeshapramud2005@gmail.com>
2026-02-21 20:52:24 +0530

    initialize data initialization service and user entity

--- src/main/java/com/wigerlabs/invoicing_system/Main.java
+++ src/main/java/com/wigerlabs/invoicing_system/Main.java
@@ -1,10 +1,13 @@
 package com.wigerlabs.invoicing_system;
 
 import com.wigerlabs.invoicing_system.config.AppConfig;
+import com.wigerlabs.invoicing_system.service.DataInitializationService;
+import com.wigerlabs.invoicing_system.util.HibernateUtil;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.hibernate.SessionFactory;
 
 import java.io.File;
 
@@ -22,6 +25,11 @@ public class Main {
             Context context = tomcat.addWebapp(CONTEXT_PATH, new File("src/main/webapp").getAbsolutePath());
             Tomcat.addServlet(context, "JerseyServlet", new ServletContainer(new AppConfig()));
             context.addServletMappingDecoded("/api/*", "JerseyServlet");
+
+            HibernateUtil.getSessionFactory();
+
+            // Initialize default data (user roles, etc.)
+            DataInitializationService.initializeDefaultData();
 
             tomcat.start();
             System.out.println("App URL: http://localhost:" + SERVER_PORT + CONTEXT_PATH);
